0d87fe7a460bb59971b82e5fbf43ba89b0f228bd
lfernando488 <lfernando488@gmail.com>
2020-12-29 02:56:13 -0300

    City and State repositories

---
+++ src/main/java/com/ajaxdev/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.ajaxdev.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ajaxdev.cursomc.domain.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
